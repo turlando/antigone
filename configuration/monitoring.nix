247a2e18ca40fb4b81db5ec2603c8a992f73710a
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-01-25 20:44:35 +0100

    Add SMART monitoring

--- configuration/monitoring.nix
+++ configuration/monitoring.nix
@@ -1,8 +1,23 @@
-{ localPkgs, config, ... }:
+{ pkgs, localPkgs, config, ... }:
 
 let
   notifyCmd = "${localPkgs.telegram-send}/bin/telegram-send";
   notifyConf = toString (config.system.statePath + /etc/telegram-send.ini);
+
+  smartdNotify = pkgs.writeShellScript "smartd-notify.sh" ''
+    set -e
+    HOSTNAME=${config.networking.hostName}
+    FROM="SMART"
+    SUBJECT="SMART error ($SMARTD_FAILTYPE) detected on $HOSTNAME"
+    MESSAGE=$(
+      echo "The following warning/error was logged by the smartd daemon:"
+      echo "$SMARTD_MESSAGE"
+      echo
+      echo "Device info:"
+      echo "$SMARTD_DEVICEINFO"
+    )
+    echo $MESSAGE | ${notifyCmd} -c ${notifyConf} -r "$FROM" -s "$SUBJECT"
+  '';
 in
 
 {
@@ -11,5 +26,10 @@ in
     ZED_EMAIL_PROG = notifyCmd;
     ZED_EMAIL_OPTS = "-c ${notifyConf} -r ZFS -s '@SUBJECT@'";
     ZED_NOTIFY_VERBOSE = true;
+  };
+
+  services.smartd = {
+    enable = true;
+    extraOptions = [ "-w ${smartdNotify.outPath}" ];
   };
 }
