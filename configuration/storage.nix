02688dbfbcb5dcdc04fc1f218a38466cc7eb7f8d
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-01-26 13:35:20 +0100

    Configure autoscrub and autotrim

--- configuration/storage.nix
+++ configuration/storage.nix
@@ -23,7 +23,7 @@ let
   systemFileSystems = util.mergeAttrsets [
     (zfsFileSystem "system" "root" "/")
     (zfsFileSystem "system" "nix" "/nix")
-    (zfsFileSystem "system" "state" config.system.statePath)
+    (zfsFileSystem "system" "state" (toString config.system.statePath))
     (zfsFileSystem "system" "home/tancredi" "/home/tancredi")
   ];
 
@@ -31,4 +31,16 @@ in
 
 {
   fileSystems = util.mergeAttrsets [ bootFileSystems systemFileSystems ];
+
+  services.zfs.autoScrub = {
+    enable = true;
+    # Run on the first Sunday of every month at 02:00.
+    interval = "Mon *-*-1..7 02:00:00";
+  };
+
+  services.zfs.trim = {
+    enable = true;
+    # Run on every Friday at 02:00.
+    interval = "Fri *-*-* 02:00:00";
+  };
 }
