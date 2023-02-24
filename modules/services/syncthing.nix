5657942a9407bad2845edfbd0002c4e1317c29b8
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-02-24 16:57:12 +0100

    Add bahnhof device and books folder to syncthing

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -9,6 +9,9 @@ let
 
   storageConfig = config.local.storage;
   storageGroup = config.users.groups.storage;
+
+  antigoneId = "6YAFIOP-Y6TGT4V-FPT77ER-YZMJODJ-533JSKV-FJ5IFOW-QNVMVAV-32XR6AR";
+  bahnhofId = "USBCMJL-WXMG4PP-XC364HB-OKBWEVH-HGKVW6E-T2YML7O-56BMQMH-3P7BUAP";
 in
 {
   fileSystems = mergeAttrsets [
@@ -42,6 +45,22 @@ in
           enable = true;
           configDir = toString dataPath;
           guiAddress = "127.0.0.1:8383";
+
+          devices = {
+            Antigone.id = antigoneId;
+            Bahnhof.id = bahnhofId;
+          };
+
+          folders = {
+            books = {
+              label = "Books";
+              path = toString storageConfig.paths.books;
+              type = "sendonly";
+              devices = [
+                config.services.syncthing.devices.Bahnhof.name
+              ];
+            };
+          };
         };
       };
   };
